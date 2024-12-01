2f1ce7838f680411ad843dcb990554c89781ea34
KOSUMOV LOMALI <groznyprogrammer@gmail.com>
2024-12-01 19:54:48 +0300

    Разбили приложение на JS модули

---
+++ quotes.js
@@ -0,0 +1,49 @@
+const quotes = [
+  {
+    quote:
+      'The only limit to our realization of tomorrow is our doubts of today',
+    author: 'Franklin D. Roosevelt',
+  },
+  {
+    quote: 'In the middle of every difficulty lies opportunity',
+    author: 'Albert Einstein',
+  },
+  {
+    quote:
+      'Happiness is not something ready made. It comes from your own actions',
+    author: 'Dalai Lama',
+  },
+  {
+    quote:
+      'Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful',
+    author: 'Albert Schweitzer',
+  },
+  {
+    quote:
+      'Do not wait to strike till the iron is hot; but make it hot by striking',
+    author: 'William Butler Yeats',
+  },
+  {
+    quote: 'Life is what happens when you’re busy making other plans',
+    author: 'John Lennon',
+  },
+  {
+    quote: 'The purpose of our lives is to be happy',
+    author: 'Dalai Lama',
+  },
+  {
+    quote: 'Get busy living or get busy dying',
+    author: 'Stephen King',
+  },
+  {
+    quote:
+      'You have within you right now, everything you need to deal with whatever the world can throw at you',
+    author: 'Brian Tracy',
+  },
+  {
+    quote: 'Believe you can and you’re halfway there',
+    author: 'Theodore Roosevelt',
+  },
+];
+
+export default quotes;
