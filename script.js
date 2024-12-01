56b6e7a372c474f5ee06b492c52f99293090a2ad
KOSUMOV LOMALI <groznyprogrammer@gmail.com>
2024-12-01 19:32:42 +0300

    Отображаем цитаты и автора отдельно. Изменили структуры массива с с массива на массив с объектами

--- script.js
+++ script.js
@@ -1,16 +1,61 @@
 const quotes = [
-  'The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt',
-  'In the middle of every difficulty lies opportunity. - Albert Einstein',
-  'Happiness is not something ready made. It comes from your own actions. - Dalai Lama',
+  {
+    quote:
+      'The only limit to our realization of tomorrow is our doubts of today',
+    author: 'Franklin D. Roosevelt',
+  },
+  {
+    quote: 'In the middle of every difficulty lies opportunity',
+    author: 'Albert Einstein',
+  },
+  {
+    quote:
+      'Happiness is not something ready made. It comes from your own actions',
+    author: 'Dalai Lama',
+  },
+  {
+    quote:
+      'Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful',
+    author: 'Albert Schweitzer',
+  },
+  {
+    quote:
+      'Do not wait to strike till the iron is hot; but make it hot by striking',
+    author: 'William Butler Yeats',
+  },
+  {
+    quote: 'Life is what happens when you’re busy making other plans',
+    author: 'John Lennon',
+  },
+  {
+    quote: 'The purpose of our lives is to be happy',
+    author: 'Dalai Lama',
+  },
+  {
+    quote: 'Get busy living or get busy dying',
+    author: 'Stephen King',
+  },
+  {
+    quote:
+      'You have within you right now, everything you need to deal with whatever the world can throw at you',
+    author: 'Brian Tracy',
+  },
+  {
+    quote: 'Believe you can and you’re halfway there',
+    author: 'Theodore Roosevelt',
+  },
 ];
 
 const quoteElement = document.getElementById('quote');
+const quoteAuthorElement = document.getElementById('quote-author');
 const generateBtn = document.getElementById('generate-btn');
 
-function generateRandomQute() {
+function generateRandomQuote() {
   const randomIndex = Math.floor(Math.random() * quotes.length);
   const randomQuote = quotes[randomIndex];
-  quoteElement.textContent = randomQuote;
+  const { quote, author: quoteAuthor } = randomQuote;
+  quoteElement.textContent = quote;
+  quoteAuthorElement.textContent = quoteAuthor;
 }
 
-generateBtn.addEventListener('click', generateRandomQute);
+generateBtn.addEventListener('click', generateRandomQuote);
